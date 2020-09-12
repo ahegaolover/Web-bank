4aee0db0efc6d8ec13a9bd45884a293db861244e
m0ood <petr.doroshenko.95@mail.ru>
2020-09-12 14:48:36 +0300

    Version 1.1 add registration and README.md

--- src/main/java/com/bank/Web/bank/controllers/SingInController.java
+++ src/main/java/com/bank/Web/bank/controllers/SingInController.java
@@ -1,13 +1,24 @@
 package com.bank.Web.bank.controllers;
 
+import com.bank.Web.bank.models.User;
+import com.bank.Web.bank.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SingInController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/singin")
-    public String home() {
+    public String home(Model model) {
+        return "singin";
+    }
+    @PostMapping("/singin")
+    public String singin(@RequestParam String Login, @RequestParam String Password,Model model) {
         return "singin";
     }
 }
--- src/main/java/com/bank/Web/bank/controllers/SingUpController.java
+++ src/main/java/com/bank/Web/bank/controllers/SingUpController.java
@@ -1,12 +1,27 @@
 package com.bank.Web.bank.controllers;
 
+import com.bank.Web.bank.models.User;
+import com.bank.Web.bank.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SingUpController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/singup")
-    public String home() {
+    public String singup(Model model) {
         return "singup";
     }
+    @PostMapping("/singup")
+    public String userAdd(@RequestParam String firstName,@RequestParam String lastName,@RequestParam String Login,@RequestParam String Password, Model model) {
+            User user = new User(firstName, lastName, Login, Password);
+            userRepository.save(user);
+            return "redirect:/singin";
+    }
 }
+
